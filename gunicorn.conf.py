6ca0f3e1847a9dde9b79bf1e8cbe856a41b0a4a1
Petre0910 <petrealmahamid@gmail.com>
2025-08-31 09:35:09 -0400

    update guicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,7 +5,7 @@ import multiprocessing
 import os
 
 # Server socket
-bind = f"0.0.0.0:{os.getenv('PORT', '8000')}"
+bind = f"127.0.0.1:{os.getenv('PORT', '8000')}"
 backlog = 2048
 
 # Worker processes
